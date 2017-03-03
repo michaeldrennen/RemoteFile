51237961904239e2cf310e5cbaeac6c5b4323ff9
Michael Drennen <michaeldrennen74@gmail.com>
2017-03-03 08:28:14 -0700

    Initial commit.

---
+++ src/RemoteFile.php
@@ -0,0 +1,47 @@
+<?php
+namespace MichaelDrennen\RemoteFile;
+
+class RemoteFile {
+
+    /**
+     * @param $url
+     * @return int|string
+     */
+    public static function getFileSize($url){
+        // Assume failure.
+        $result = -1;
+
+        $curl = curl_init( $url );
+
+        // Issue a HEAD request and follow any redirects.
+        curl_setopt( $curl, CURLOPT_NOBODY, true );
+        curl_setopt( $curl, CURLOPT_HEADER, true );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
+        //curl_setopt( $curl, CURLOPT_USERAGENT, get_user_agent_string() );
+
+        $data = curl_exec( $curl );
+        curl_close( $curl );
+
+        if( $data ) {
+            $content_length = "unknown";
+            $status = "unknown";
+
+            if( preg_match( "/^HTTP\/1\.[01] (\d\d\d)/", $data, $matches ) ) {
+                $status = (int)$matches[1];
+            }
+
+            if( preg_match( "/Content-Length: (\d+)/", $data, $matches ) ) {
+                $content_length = (int)$matches[1];
+            }
+
+            // http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+            if( $status == 200 || ($status > 300 && $status <= 308) ) {
+                $result = $content_length;
+            }
+        }
+
+        return $result;
+    }
+    
+}
