ef762e1a5d9c8888b643b0f38d0ee199dc3b9e99
Michael Drennen <michaeldrennen74@gmail.com>
2017-03-04 07:59:35 -0700

    Large file.

--- src/RemoteFile.php
+++ src/RemoteFile.php
@@ -65,7 +65,14 @@ class RemoteFile {
     public static function humanFileSize(int $bytes, int $decimals = 2) : string {
         $size = ['B','kB','MB','GB','TB','PB','EB','ZB','YB'];
         $factor = (int)floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+
+        $unit = @$size[$factor];
+
+        if($unit == 'B'){
+            return $bytes . $unit;
+        }
+
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . $unit;
     }
     
 }
--- tests/RemoteFileTest.php
+++ tests/RemoteFileTest.php
@@ -4,7 +4,21 @@ use MichaelDrennen\RemoteFile\RemoteFile;
 
 class RemoteFileTest extends TestCase {
     public function testInvalidUrl() {
-        $remoteFileSize = RemoteFile::getFileSize('notValidUrl');
+        $url = 'invalidUrl';
+        $remoteFileSize = RemoteFile::getFileSize($url);
+        $this->assertEquals($remoteFileSize, -1);
+    }
+
+    public function testDarthPlagueisTheWiseUrlInBytes() {
+        $url = 'https://raw.githubusercontent.com/michaeldrennen/RemoteFile/master/darthPlagueisTheWise.txt';
+        $remoteFileSize = RemoteFile::getFileSize($url);
+        $this->assertEquals($remoteFileSize, 747);
+    }
+
+    public function testHumanReadableResultUnder1kB() {
+        $url = 'https://raw.githubusercontent.com/michaeldrennen/RemoteFile/master/darthPlagueisTheWise.txt';
+        $remoteFileSize = RemoteFile::getHumanReadableFileSize($url);
+        $this->assertEquals($remoteFileSize, '747B');
     }
 
 }
